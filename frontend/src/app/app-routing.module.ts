dfd18e428a4218e7e55a64db6b13fafa8cd3cd53
Ramon <ramonfranklyn@gmail.com>
2021-12-07 10:50:58 -0300

    Evolução do projeto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent  } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductsCreateComponent } from './components/products/products-create/products-create.component';
+
+const routes: Routes = [
+  {
+  path: "",
+  component: HomeComponent
+ },
+ {
+   path: "products",
+   component: ProductCrudComponent
+ },
+ {
+   path: "products/create",
+   component: ProductsCreateComponent
+ }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
