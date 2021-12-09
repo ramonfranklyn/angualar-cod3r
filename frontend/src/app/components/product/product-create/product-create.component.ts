fed5341f61b71cc642d70dd7a6c7f6aa1bb40d99
Ramon <ramonfranklyn@gmail.com>
2021-12-09 10:35:11 -0300

    Criação inicial do compnente product read

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -12,7 +12,7 @@ export class ProductCreateComponent implements OnInit {
 
   product: Product = {
     name: '',
-    price: 0.00
+    price: undefined
   }
 
   constructor(private productService: ProductService,
